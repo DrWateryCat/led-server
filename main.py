576bd32809046e9e3ef1903ba8a820b6c522adb6
DrWateryCat <drwaterycat@gmail.com>
2017-07-29 02:33:43 -0400

    Fixed indentation

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ import socket
 
 
 # LED strip configuration:
-LED_COUNT      = 16      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_COUNT = 16      # Number of LED pixels.
+LED_PIN = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+# True to invert the signal (when using NPN transistor level shift)
+LED_INVERT = False
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
 CURRENT_COLOR = Color(255, 0, 0)
 CURRENT_COMMAND = 0
-
 
 
 # Define functions which animate LEDs in various ways.
@@ -32,18 +32,20 @@ def colorWipe(strip, color, wait_ms=50):
 	for i in range(strip.numPixels()):
 		strip.setPixelColor(i, color)
 		strip.show()
-		time.sleep(wait_ms/1000.0)
+		time.sleep(wait_ms / 1000.0)
+
 
 def theaterChase(strip, color, wait_ms=50, iterations=10):
 	"""Movie theater light style chaser animation."""
 	for j in range(iterations):
 		for q in range(3):
 			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, color)
+				strip.setPixelColor(i + q, color)
 			strip.show()
-			time.sleep(wait_ms/1000.0)
+			time.sleep(wait_ms / 1000.0)
 			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, 0)
+				strip.setPixelColor(i + q, 0)
+
 
 def wheel(pos):
 	"""Generate rainbow colors across 0-255 positions."""
@@ -56,32 +58,36 @@ def wheel(pos):
 		pos -= 170
 		return Color(0, pos * 3, 255 - pos * 3)
 
+
 def rainbow(strip, wait_ms=20, iterations=1):
 	"""Draw rainbow that fades across all pixels at once."""
-	for j in range(256*iterations):
+	for j in range(256 * iterations):
 		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((i+j) & 255))
+			strip.setPixelColor(i, wheel((i + j) & 255))
 		strip.show()
-		time.sleep(wait_ms/1000.0)
+		time.sleep(wait_ms / 1000.0)
+
 
 def rainbowCycle(strip, wait_ms=20, iterations=5):
 	"""Draw rainbow that uniformly distributes itself across all pixels."""
-	for j in range(256*iterations):
+	for j in range(256 * iterations):
 		for i in range(strip.numPixels()):
 			strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
 		strip.show()
-		time.sleep(wait_ms/1000.0)
+		time.sleep(wait_ms / 1000.0)
+
 
 def theaterChaseRainbow(strip, wait_ms=50):
 	"""Rainbow movie theater light style chaser animation."""
 	for j in range(256):
 		for q in range(3):
 			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, wheel((i+j) % 255))
+				strip.setPixelColor(i + q, wheel((i + j) % 255))
 			strip.show()
-			time.sleep(wait_ms/1000.0)
+			time.sleep(wait_ms / 1000.0)
 			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, 0)
+				strip.setPixelColor(i + q, 0)
+
 
 def tcp_server():
     sock = socket.socket(family=AF_INET, type=SOCK_STREAM)
@@ -107,7 +113,8 @@ def tcp_server():
 # Main program logic follows:
 if __name__ == '__main__':
 	# Create NeoPixel object with appropriate configuration.
-	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ,
+	                          LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 	# Intialize the library (must be called once before other functions).
 	strip.begin()
 
